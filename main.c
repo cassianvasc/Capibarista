f5e9724dcbfbeb17debb89f444de6b2663568ad8
EduardaSouzaOL <messtudies18@example.com>
2026-05-19 15:38:18 -0300

    tamanho tela principal + background image

--- main.c
+++ main.c
@@ -3,26 +3,36 @@
 #include "lista_encadeada.h"
 
 int main(){
-    InitWindow(GetMonitorWidth(0), GetMonitorHeight(0), "Capibarista");
+
+    const int LARGURA_TELA = 1280;
+    const int ALTURA_TELA = 720;
+    
+
+    InitWindow(LARGURA_TELA, ALTURA_TELA, "Capibarista");
 
     SetTargetFPS(60);
 
     Jogo jogo;
-
     inicializarJogo(&jogo);
+    
+
 
     while(WindowShouldClose() == false){
-
         atualizarJogo(&jogo);
 
         BeginDrawing();
             ClearBackground(BEIGE);
+            
+
+            
             desenharJogo(&jogo);
         EndDrawing();
     }
 
     liberarLista(&jogo.listaClientes);
+    
 
+    
     CloseWindow();
     return 0;
 }
